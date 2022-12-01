34baf2c4ad9c738dfa251b8a42d8eed8d74f657f
Aaron  Jones <jon17049@byui.edu>
2022-12-01 15:05:20 -0700

    Signed-off-by: Aaron  Jones <jon17049@byui.edu>

--- library.py
+++ library.py
@@ -1,5 +1,4 @@
 from scene import *
-# from math_generator import *
 from door import *
 from key import *
 from light import *
@@ -11,7 +10,7 @@ class Library(Scene):
 
     def __init__(self, image):
         super().__init__(image)
-        # all the groups in room 1
+        # all the groups in room 2
 
         self.font = pygame.font.SysFont('timesnewroman', 30)
 
@@ -20,34 +19,46 @@ class Library(Scene):
         self.doorGroup = pygame.sprite.Group()
         self.lightsGroup = pygame.sprite.Group()
         self.pensGroup = pygame.sprite.Group()
-        self.walls = GameSprite(LIBRARY_WALLS, 0, (990,800))
+        self.walls = GameSprite(LIBRARY_WALLS, 0, (990, 800))
         self.walls.mask = pygame.mask.from_surface(self.walls.image)
 
+        self.answers = 0  # added and needs to be connected to other variables
+        self.passed = False  # added and needs to be connected to other variables
 
     def addItemTo(self, item, position, group):
         return super().addItemTo(item, position, group)
 
     def create_library(self, callback):
         callback.library_door = Door(DOOR_IMAGE, DOOR_SIZE)
-        callback.key_science = Key(KEY_IMAGE, ITEM_SIZE)
+        # changed wording and needs to be connected to other variables
+        callback.key_library = Key(KEY_IMAGE, ITEM_SIZE)
 
     def init_library(self, callback):
 
+        # added and needs to be connected to other variables
+        self.ques, self.ans = self.questions.generate()
 
-        
         callback.library_door.init_door(self, callback.hallway, (513, 600), (712, 185),
-                                "To the hallway.")
+                                        "To the hallway.")
         callback.library_door.locked = False
 
-        
-        callback.key_science.init_key(self, callback.door_to_science, (500, 500), "This is the key to science")
+        # Create the loading lights on the white board
 
+        # added and needs to be connected to other variables
+        self.loadingLight1.init_light(self, (330, 12))
+
+        # callback.key_science.init_key(
+        #     self, callback.door_to_science, (500, 500), "This is the key to science")
 
     def display_question(self, callback):
         question_output = self.font.render(self.ques, True, BLACK)
         callback._screen.blit(question_output, (SCREEN_RECT.centerx - 30, 40))
 
-
     def reset_question(self):
         self.ques, self.ans = self.questions.generate()
 
+    # added and needs to be connected to other variables
+    def pass_room(self, callback):
+        callback.key_library = Key(KEY_IMAGE, ITEM_SIZE)
+        callback.key_library.init_key(self, callback.door_to_library, (600, 400),
+                                      "This is the key to open the science door")
---
+++ word_generator.py
@@ -0,0 +1,26 @@
+import json
+import random
+
+
+class Generator():
+
+    def __init__(self, filename):
+        self.data = self.open_file(filename)
+        self.filename = filename
+
+    def open_file(self, filename):
+        with open(filename+".json", "r") as my_file:
+            if not my_file:
+                print("file open failed")
+            data = my_file.read()
+
+        obj = json.loads(data)
+        obj = list(obj.items())
+        return obj
+
+    def generate(self):
+        if len(self.data) == 0:
+            self.data = self.open_file(self.filename)
+        choice = random.randint(0, len(self.data) - 1)
+        que, ans = self.data.pop(choice)
+        return que, ans
